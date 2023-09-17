b6192cbe4e8b403ac7c26a92150c1fa4a4d29a7e
Gyokujin <dd43454548@naver.com>
2023-09-18 01:36:10 +0900

    # 깃허브 업로드

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+// 저장하는 방법
+// 1. 저장할 데이터가 존재
+// 2. 데이터를 제이슨으로 변환
+// 3. 제이슨을 외부에 저장
+
+// 불러오는 방법
+// 1. 외부에 저장된 제이슨을 가져옴
+// 2. 제이슨을 데이터형태로 변환
+// 3. 불러온 데이터를 사용
+
+public class PlayerData
+{
+    // 이름, 레벨, 코인, 착용중인 무기
+    public string name;
+    public int level = 1;
+    public int coin = 100;
+    public int item = -1;
+}
+
+public class DataManager : MonoBehaviour
+{
+    // 싱글톤
+    public static DataManager instance;
+
+    public PlayerData nowPlayer = new PlayerData();
+
+    public string path;
+    public int nowSlot;
+
+    private void Awake()
+    {
+        #region 싱글톤
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(instance.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        #endregion
+
+        path = Application.persistentDataPath + "/save";
+
+    }
+
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(nowPlayer);
+        File.WriteAllText(path + nowSlot.ToString(), data);
+    }
+
+    public void LoadData()
+    {
+        string data = File.ReadAllText(path + nowSlot.ToString());
+        nowPlayer = JsonUtility.FromJson<PlayerData>(data);
+    }
+
+    public void DataClear()
+    {
+        nowSlot = -1;
+        nowPlayer = new PlayerData();
+    }
+}
---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class Select : MonoBehaviour
+{
+    public GameObject creat;
+    public Text[] slotText;
+    public Text newPlayerName;
+
+    bool[] savefile = new bool[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 슬롯별로 저장된 데이터가 존재하는지 판단.
+        for (int i = 0; i < 3; i++)
+        {
+            if (File.Exists(DataManager.instance.path + $"{i}"))
+            {
+                savefile[i] = true;
+                DataManager.instance.nowSlot = i;
+                DataManager.instance.LoadData();
+                slotText[i].text = DataManager.instance.nowPlayer.name;
+            }
+            else
+            {
+                slotText[i].text = "비어있음";
+            }
+        }
+        DataManager.instance.DataClear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // 슬롯이 3개인데 어떻게 알맞게 불러오느냐?
+    public void Slot(int number)
+    {
+        DataManager.instance.nowSlot = number;
+
+        if (savefile[number])
+        {
+            DataManager.instance.LoadData();
+            GoGame();
+        }
+        else
+        {
+            Creat();
+        }
+    }
+
+    public void Creat()
+    {
+        creat.gameObject.SetActive(true);
+    }
+
+    public void GoGame()
+    {
+        if (!savefile[DataManager.instance.nowSlot])
+        {
+            DataManager.instance.nowPlayer.name = newPlayerName.text;
+            DataManager.instance.SaveData();
+        }
+
+        SceneManager.LoadScene(1);
+    }
+}
